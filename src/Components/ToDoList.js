928c400c23ea4fb4ada317bbada742939284b6bb
Shivam <me210003070@iiti.ac.in>
2023-07-13 13:15:02 +0530

    added authentication in todolist

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { useAuth0 } from "@auth0/auth0-react";
-
+import { useAuth0 ,withAuthenticationRequired} from "@auth0/auth0-react";
 function ToDoList() {
   const {user, loginWithRedirect, isAuthenticated, logout} = useAuth0();
-    
   const email=user.email;
   Axios.post("http://localhost:8000/todolist",{
-      email
+        email
   })
   .then(res=>{
-    console.log(res);
-  })
-  .catch(e=>{
-      alert("wrong details");
-      console.log(e);
-  })
+      console.log(res.data);
+    })
+    .catch(e=>{
+        alert("wrong details");
+        console.log(e);
+    })
+  
   return (
     <div>
       
@@ -23,4 +22,6 @@ function ToDoList() {
   )
 }
 
-export default ToDoList
+export default withAuthenticationRequired(ToDoList,{
+  returnTo: '/'
+});
